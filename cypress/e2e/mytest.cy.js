a0f1f403251d0c3a25d84a813954a63e8c7b00a7
VijayVarala <varalavijay18@gmail.com>
2025-02-18 11:48:25 +0530

    cypress implementation

---
+++ cypress/e2e/mytest.cy.js
@@ -0,0 +1,13 @@
+describe('sample test', () => {
+  it('login', () => {
+    cy.visit("http://austin-web-react.s3-website.ap-south-1.amazonaws.com/login/false", { failOnStatusCode: false });
+    cy.get("#email").type("vijayinitiator@gmail.com")
+    cy.get("#password").type("1234567890")
+    cy.get(".signIn-btn").click()
+    cy.url().should('eq','http://austin-web-react.s3-website.ap-south-1.amazonaws.com/dashboard')
+    cy.title().should('include','Austin')
+
+
+
+  });
+});
